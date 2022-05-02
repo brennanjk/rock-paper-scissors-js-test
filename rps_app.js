0b3b4755998ae74f594aabc35813b58a79715ec6
Brennan Kilpatrick <brennankilpatrick@gmail.com>
2022-05-02 15:35:39 -0700

    Add global variable array with possible choices

--- rps_app.js
+++ rps_app.js
@@ -1,4 +1,9 @@
+/* Create global array variable for possible game selections*/
+
+const choices = ['rock','paper','scissors'];
+
 /* Function (computerPlay) to choose a computer choice that randomly chooses from 'rock', 'paper', or 'scissors' and assign that choice to computerSelection variable*/
+
 
 /* Function (playRound) to play a single round. It should run the computerPlay function above to generate the computer's choice, plus prompt the user to enter a choice*/
 /* It should then compare the choices and determine/display the result */
