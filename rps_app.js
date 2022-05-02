312d4258a9c9ecb68882591a63863d50781cfa09
Brennan Kilpatrick <brennankilpatrick@gmail.com>
2022-05-02 15:50:58 -0700

    Create computerPlay function
    Created a computer play function that randomly selects an object from the 'choices' array and assigns it to a computerSelection variable

--- rps_app.js
+++ rps_app.js
@@ -4,6 +4,16 @@ const choices = ['rock','paper','scissors'];
 
 /* Function (computerPlay) to choose a computer choice that randomly chooses from 'rock', 'paper', or 'scissors' and assign that choice to computerSelection variable*/
 
+function computerPlay() {
+    // generate random index between 0 and 2
+    const randomIndex = Math.floor(Math.random() * choices.length);
+
+    //Use randomIndex to assign an object from our choices array to computerSelection variable
+    const computerSelection = choices[randomIndex];
+
+    //print computerSelection for testing purposes
+    console.log(computerSelection)
+}
 
 /* Function (playRound) to play a single round. It should run the computerPlay function above to generate the computer's choice, plus prompt the user to enter a choice*/
 /* It should then compare the choices and determine/display the result */
