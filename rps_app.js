dbeb00074cf1bb8210372c25b3ea1af43b93cac4
Brennan Kilpatrick <brennankilpatrick@gmail.com>
2022-05-02 17:02:39 -0700

    Add playRound function to accept player and computer choices and compare them to determine winner
    Added if/else if statements with nested if/else if statements to compare possible outcomes and console.log the result

--- rps_app.js
+++ rps_app.js
@@ -15,4 +15,53 @@ function computerPlay() {
 /* Function (playRound) to play a single round. It should run the computerPlay function above to generate the computer's choice, plus prompt the user to enter a choice*/
 /* It should then compare the choices and determine/display the result */
 
+function playRound(playerSelection, computerSelection) {
+    
+    //if player chooses rock, check what the computer chose
+    if (playerSelection === 'rock') {
+
+        //possible outcomes based on computer choices
+        if (computerSelection === 'paper') {
+            console.log(`${computerSelection} beats ${playerSelection}; the computer wins!`)
+        }
+        else if (computerSelection === 'scissors') {
+            console.log(`${playerSelection} beats ${computerSelection}; you win!`)
+        }
+        else if (computerSelection === 'rock') {
+            console.log(`Both players chose ${playerSelection}; it's a tie!`)
+        }
+    }
+
+    //if player chooses paper, check what the computer chose
+    else if (playerSelection === 'paper') {
+
+        //possible outcomes based on computer choices
+        if (computerSelection === 'scissors') {
+            console.log(`${computerSelection} beats ${playerSelection}; the computer wins!`)
+        }
+        else if (computerSelection === 'rock') {
+            console.log(`${playerSelection} beats ${computerSelection}; you win!`)
+        }
+        else if (computerSelection === 'paper') {
+            console.log(`Both players chose ${playerSelection}; it's a tie!`)
+        } 
+    }
+
+    //if player chooses scissors, check what the computer chose
+    else if (playerSelection === 'scissors') {
+
+        //possible outcomes based on computer choices
+        if (computerSelection === 'rock') {
+            console.log(`${computerSelection} beats ${playerSelection}; the computer wins!`)
+        }
+        else if (computerSelection === 'paper') {
+            console.log(`${playerSelection} beats ${computerSelection}; you win!`)
+        }
+        else if (computerSelection === 'scissors') {
+            console.log(`Both players chose ${playerSelection}; it's a tie!`)
+        } 
+    }
+
+}
+
 /* Function (game) that calls our playRound function for a 5 round game. It will keep score and declare a winner at the end */
